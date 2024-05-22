063dfdbb39907c23e8b9d93e08d10270e36af28c
IOleg-sus <guchkooleg7@gmail.com>
2024-05-22 19:44:42 +0300

    refactoring my bot

--- main.py
+++ main.py
@@ -7,48 +7,21 @@ bot = telebot.TeleBot('6895824327:AAEyCfrTRh-7wGuIjrAVSe9y2gXxx1Vpunk')
 @bot.message_handler(commands=['start'])
 
 def start(message):
-    bot.send_message(message.chat.id, 'Hello, ' + message.from_user.first_name + message.from_user.last_name)
-    markup = types.ReplyKeyboardMarkup()
-    btn = types.KeyboardButton('Clear chat ')
-    btn1 = types.KeyboardButton('Show author github')
-    markup.add(btn) 
-    markup.add(btn1)
-    bot.send_message(message.chat.id, 'Chat cleared' , reply_markup=markup)
-    bot.register_next_step_handler(message, on_click)
+    local_name = f'Hello, <b>{message.from_user.first_name + message.from_user.last_name}</b>'
+    bot.send_message(message.chat.id , local_name , parse_mode = 'html')
     
-def on_click(message):
-    if message.text == 'Clear chat':
-       bot.delete_message(message.chat.id, message.message_id - 1)
-    elif message.text == 'Show author github':
-        bot.send_message(message.chat.id, 'That author`s website: https://github.com/IOleg-crypto/python-telegram-bot')
-@bot.message_handler(commands=['setnumber'])
-
-def setnumber(message):
-    bot.send_message(message.chat.id, 5)
-   
+@bot.message_handler()
+def get_user_text(message):
+    if message.text == "Hello":
+        bot.send_message(message.chat.id, "Hello")
+    elif message.text == "id":
+         bot.send_message(message.chat.id,f'Your id : {message.from_user.id}')
+    else:
+        bot.send_message(message.chat.id, "I don`t understand you")
+          
 @bot.message_handler(commands=['website'])
-
 def site(message):
-    bot.send_message(message.chat.id, "That author`s website: https://github.com/IOleg-crypto/python-telegram-bot")
+    bot.send_message(message.chat.id, f"That author`s website: {https://github.com/IOleg-crypto/python-telegram-bot}")
    
-@bot.message_handler(commands=['getbutton'])
-def getbutton(message):
-    markup = types.InlineKeyboardMarkup()
-    btn1 = types.InlineKeyboardButton('GitHub', url='https://github.com/IOleg-crypto/python-telegram-bot')
-    #params : use only '' because you don`t see button`
-    markup.row(btn1)
-    bot.reply_to(message, "That author`s website", reply_markup=markup)
-   
-@bot.message_handler(commands=['clearChat'])
-def clearChat(message):
-    markup = types.InlineKeyboardMarkup()
-    btn2 = types.InlineKeyboardButton('Clear chat', callback_data = 'delete')
-    markup.add(btn2)
-    #bot.reply_to(message, "Chat cleared", reply_markup=markup)
 
-@bot.callback_query_handler(func= lambda callback: True)
-def callback_message(callback):
-    if callback.data == 'delete':
-       bot.delete_message(callback.message.chat.id, callback.message.message_id - 2)
-
-bot.infinity_polling()
+bot.polling(none_stop = True)
